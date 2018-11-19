3d579d00d11d1562fcb84a499b1987f07b6fb919
Unknown <julianne.loria@valpo.edu>
2018-11-19 16:53:46 -0600

    Created base functionality
    Finished model, created form with inputs and display for change

--- MoneyChanger/Controllers/HomeController.cs
+++ MoneyChanger/Controllers/HomeController.cs
@@ -3,27 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MoneyChanger.Models;
 
 namespace MoneyChanger.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
-
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult Index(decimal moneyIn)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
+            ViewBag.ChangeList = Changer.MakeChange(moneyIn);
             return View();
         }
     }
--- MoneyChanger/Models/Changer.cs
+++ MoneyChanger/Models/Changer.cs
@@ -8,17 +8,114 @@ namespace MoneyChanger.Models
     public static class Changer
     {
         public static List<Change> MakeChange(decimal moneyIn) {
-            return new List<Change>();
+            List<Change> changeList = new List<Change>();
+            var remainder = moneyIn;
+
+            while (remainder > 0)
+            {
+                // check divisibility 
+                if(remainder / MoneyType.halfDollar >= 1)
+                {
+                    var calc = remainder / MoneyType.halfDollar;
+
+                    // create new change object based on calculation
+                    var change = new Change();
+                    change.amount = MoneyType.halfDollar;
+                    change.quantity = Convert.ToInt32(Math.Floor(calc));
+
+                    // add change value for return
+                    changeList.Add(change);
+
+                    // adjust remainder
+                    remainder -= change.quantity * MoneyType.halfDollar;
+                } else if (remainder / MoneyType.quarter >= 1)
+                {
+                    var calc = remainder / MoneyType.quarter;
+
+                    // create new change object based on calculation
+                    var change = new Change();
+                    change.amount = MoneyType.quarter;
+                    change.quantity = Convert.ToInt32(Math.Floor(calc));
+
+                    // add change value for return
+                    changeList.Add(change);
+
+                    // adjust remainder
+                    remainder -= change.quantity * MoneyType.quarter;
+                } else if (remainder / MoneyType.dime >= 1)
+                {
+                    var calc = remainder / MoneyType.dime;
+
+                    // create new change object based on calculation
+                    var change = new Change();
+                    change.amount = MoneyType.dime;
+                    change.quantity = Convert.ToInt32(Math.Floor(calc));
+
+                    // add change value for return
+                    changeList.Add(change);
+
+                    // adjust remainder
+                    remainder -= change.quantity * MoneyType.dime;
+                } else if (remainder / MoneyType.nickel >= 1)
+                {
+                    var calc = remainder / MoneyType.nickel;
+
+                    // create new change object based on calculation
+                    var change = new Change();
+                    change.amount = MoneyType.nickel;
+                    change.quantity = Convert.ToInt32(Math.Floor(calc));
+
+                    // add change value for return
+                    changeList.Add(change);
+
+                    // adjust remainder
+                    remainder -= change.quantity * MoneyType.nickel;
+                } else if (remainder / MoneyType.penny >= 1)
+                {
+                    var calc = remainder / MoneyType.penny;
+
+                    // create new change object based on calculation
+                    var change = new Change();
+                    change.amount = MoneyType.penny;
+                    change.quantity = Convert.ToInt32(Math.Floor(calc));
+
+                    // add change value for return
+                    changeList.Add(change);
+
+                    // adjust remainder
+                    remainder -= change.quantity * MoneyType.penny;
+                }
+            }
+            return changeList;
+        }
+
+        public static string GetName(decimal moneyAmount)
+        {
+            switch (moneyAmount)
+            {
+                case MoneyType.halfDollar:
+                    return "Half Dollar";
+                case MoneyType.quarter:
+                    return "Quarter";
+                case MoneyType.dime:
+                    return "Dime";
+                case MoneyType.nickel:
+                    return "Nickel";
+                case MoneyType.penny:
+                    return "Penny";
+                default:
+                    return "";
+            }
         }
     }
 
     public struct MoneyType
     {
-        public static decimal halfDollar = .5M;
-        public static decimal quarter = .25M;
-        public static decimal dime = .1M;
-        public static decimal nickel = .05M;
-        public static decimal penny = .01M;
+        public const decimal halfDollar = .5M;
+        public const decimal quarter = .25M;
+        public const decimal dime = .1M;
+        public const decimal nickel = .05M;
+        public const decimal penny = .01M;
     }
 
     public class Change
